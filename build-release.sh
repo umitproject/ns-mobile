16d8050736847e804ef68543a7ad395b53dcb2e2
Luís A. Bastião Silva <luis.kop@gmail.com>
2012-09-06 15:51:32 +0100

    Added build release command

---
+++ build-release.sh
@@ -0,0 +1,11 @@
+android update project --name ns-mobile --target android-10 --path .
+echo "Building Native"
+ndk-build
+cp ./libs/armeabi/scanner ./res/raw/archived/scanner
+cd ./res/raw/archived
+rm archive.zip
+zip -r archive *
+mv archive.zip ../archive
+cd ../../../
+echo "Building Android Project"
+ant release
